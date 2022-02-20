c73ee72062098d1f55c565fa85195490e6ec2a44
Thales Tavares <thalestav41@gmail.com>
2022-02-20 15:01:18 -0300

    add js page home

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,43 @@
 const myModal = new bootstrap.Modal("#register-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+checkLogged();
+//30 minutos de trabalho
+//logar no sistema
+document.getElementById("login-form").addEventListener("submit", function(e){
+e.preventDefault();
+const email = document.getElementById("email-input").value;
+const password = document.getElementById("password-input").value;
+const checkSession = document.getElementById("session-check").checked;
+
+const account = getAccount(email);
+
+if(!account) {
+    alert("Opps! Verifique o usuário ou a senha.");
+    return;
+}
+
+if(account) {
+    if(account.password !== password) {
+        alert("Opps! Verifique o usuário ou a senha.");
+        return;
+    }  
+
+    saveSession(email, checkSession )
+
+    window.location.href = "home.html";
+
+}
+
+
+});
 
 //criar conta
 document.getElementById("create-form").addEventListener("submit", function(e) {
     e.preventDefault();
 
-    const email =document.getElementById("email-create-input").value;
-    const password =document.getElementById("password-create-input").value;
+    const email = document.getElementById("email-create-input").value;
+    const password = document.getElementById("password-create-input").value;
     
     if(email.length < 5) {
         alert("Preencha o campo com um E-mail válido");
@@ -27,6 +59,35 @@ document.getElementById("create-form").addEventListener("submit", function(e) {
 
 });
 
+function checkLogged() {
+    if(session){
+        sessionStorage.setItem("logged", session);
+        logged = session;
+    }
+
+    if(logged){
+        saveSession(logged, session);
+
+        window.location.href = "home.html";
+    }
+}
+
 function saveAccount (data) {
     localStorage.setItem(data.login,JSON.stringify(data));
+}
+
+function saveSession(data, saveSession) {
+    if(saveSession){
+        localStorage.setItem("session", data);
+    }
+    sessionStorage.setItem("logged", data);
+}
+
+function getAccount(key) {
+    const account = localStorage.getItem(key);
+
+    if(account){
+        return JSON.parse(account);
+    }
+    return "";
 }
